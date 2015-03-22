15155a66ee3480a4c4d2069dee1bebd4e6f9f3f2
Muhummad Patel <muhummad.patel@gmail.com>
2015-03-22 18:04:22 +0200

    Use List of devices instead of hash map. Handle WakeUp interrupts. Implement IORequest and TerminateProcess syscalls.

--- src/KernelImp.java
+++ src/KernelImp.java
@@ -1,19 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * muhummad        15/03/19.
  */
 public class KernelImp implements Kernel {
 
-    private int timeslice;
+    int timeslice;
 
     //<device id, queue of pids waiting for device with device id>
-    private HashMap<Integer, Queue<ProcessControlBlock>> deviceQueues = new HashMap<Integer, Queue<ProcessControlBlock>>();
+    private ArrayList<IODeviceImp> devices = new ArrayList<IODeviceImp>();
     private Queue<ProcessControlBlock> ready = new LinkedList<ProcessControlBlock>();
 
 
@@ -28,29 +25,34 @@ public class KernelImp implements Kernel {
                 ProcessControlBlock switchedIn = ready.poll();
                 ProcessControlBlock switchedOut = Simulator.cpu.contextSwitch(switchedIn);
                 ready.add(switchedOut);
-                Simulator.timer.scheduleInterrupt(timeslice, switchedIn.getPID());
+                Simulator.timer.scheduleInterrupt(timeslice, switchedIn);
                 break;
             case InterruptHandler.WAKE_UP:
-                int deviceId = Integer.parseInt((String)varargs[0]);
-                int pid = Integer.parseInt((String)varargs[1]);
+                int deviceId = (Integer)(varargs[0]);
+                int pid = (Integer)(varargs[1]);
 
-                LinkedList<ProcessControlBlock> deviceQueue = (LinkedList<ProcessControlBlock>)deviceQueues.get(deviceId);
+                LinkedList<ProcessControlBlock> relQueue = new LinkedList<ProcessControlBlock>();
+                for(IODeviceImp iod: devices){
+
+                    if(iod.getID() == deviceId){
+                        relQueue = iod.deviceQueue;
+                    }
+                }
+
                 int pos = -1;
-                for(int i = 0; i < deviceQueue.size(); i++){
-                    ProcessControlBlock pcb = deviceQueue.get(i);
+                for(int i = 0; i < relQueue.size(); i++){
+                    ProcessControlBlock pcb = relQueue.get(i);
                     if(pcb.getPID() == pid){
                         pos = i;
                         break;
                     }
                 }
 
-                //TODO: some error checking?
                 if(pos != -1){
-                    ProcessControlBlock removed = deviceQueue.remove(pos);
-                    removed.nextInstruction();
-                    ready.add(removed);
+                    ProcessControlBlock relProcess = relQueue.remove(pos);
+                    relProcess.nextInstruction();
+                    ready.add(relProcess);
                 }
-
                 break;
         }
     }
@@ -68,10 +70,12 @@ public class KernelImp implements Kernel {
             case IO_REQUEST:
                 //TODO: IO_REQUEST
                 System.out.println("syscall IO REQ");
+                sys_io_request((Integer)(varargs[0]), (Integer)(varargs[1]));
                 break;
             case TERMINATE_PROCESS:
                 //TODO: TERMINATE PROCESS
                 System.out.println("syscall TERMINATE PROC");
+                sys_terminate_process();
                 break;
             default:
                 return 1;
@@ -84,10 +88,10 @@ public class KernelImp implements Kernel {
     //TODO: make these return int?
     private void sys_make_device(int deviceId, String deviceType){
         IODeviceImp dev = new IODeviceImp(deviceId, deviceType);
-        deviceQueues.put(dev.getID(), new LinkedList<ProcessControlBlock>());
+        devices.add(dev);
 
         System.out.println(dev);
-        System.out.println(deviceQueues.size());
+        System.out.println(devices.size());
     }
 
     private void sys_execve(String programFilename){
@@ -105,7 +109,7 @@ public class KernelImp implements Kernel {
                 if(instructionType.equalsIgnoreCase("CPU")){
                     int instructionDuration = Integer.parseInt(data[1]);
                     pcb.addInstruction(new CPUInstruction(instructionDuration));
-                }else if(instructionType.equalsIgnoreCase("I/O") || data[1].equalsIgnoreCase("IO")){
+                }else if(instructionType.equalsIgnoreCase("I/O") || instructionType.equalsIgnoreCase("IO")){
                     int instructionDuration = Integer.parseInt(data[1]);
                     int deviceId = Integer.parseInt(data[2]);
                     pcb.addInstruction(new IOInstruction(instructionDuration, deviceId));
@@ -117,5 +121,44 @@ public class KernelImp implements Kernel {
         }catch(FileNotFoundException e){
             System.out.println(e.getMessage() + "\nError loading program: " + programFilename);
         }
+    }
+
+    private void sys_io_request(int deviceId, int duration){
+        ProcessControlBlock curr = Simulator.cpu.getCurrentProcess();
+
+        for(IODevice iod: devices) {
+            if(iod.getID() == deviceId){
+                long completeTime = iod.requestIO(duration, curr);
+                Simulator.eventQueue.removeTimeoutEvent(curr.getPID());
+                Simulator.eventQueue.add(new WakeUpEvent(completeTime, iod, curr));
+                break;
+            }
+        }
+
+        ProcessControlBlock next = ready.poll();
+        Simulator.cpu.contextSwitch(next);
+        if(next != null){
+            Simulator.timer.scheduleInterrupt(timeslice, next);
+        }
+    }
+
+    private void sys_terminate_process(){
+        ProcessControlBlock curr = Simulator.cpu.getCurrentProcess();
+        Simulator.eventQueue.removeTimeoutEvent(curr.getPID());
+
+        ProcessControlBlock next = ready.poll();
+        Simulator.cpu.contextSwitch(next);
+
+        if(next != null){
+            Simulator.timer.scheduleInterrupt(timeslice, next);
+        }
+    }
+
+    public ProcessControlBlock nextProcess(){
+        return ready.poll();
+    }
+
+    public void addToReady(ProcessControlBlock pcb) {
+        ready.add(pcb);
     }
 }
